7f34054a66531c9c97ef93bd06ea5b080fe08b33
Sujon <abdur.sujon.dev@gmail.com>
2025-12-20 23:24:41 +0000

    Remove  comment in TypeCasting.java

--- src/Chapter1_DataTypesVariableArrays/TypeCasting.java
+++ src/Chapter1_DataTypesVariableArrays/TypeCasting.java
@@ -2,7 +2,6 @@ package Chapter1_DataTypesVariableArrays;
 
 public class TypeCasting {
     public static void main (String [] args){
-        //Baron shoot into library to follow my dick cause he likes black cock
         //byte < short < int < long < float < double
         double d = 55555.66d;
         float f = (float)d;
